949ffe141603578950e24e01b3652e8a9a8dc170
amolTelang <telangamol0909@gmail.com>
2021-08-07 15:41:09 +0530

    added list component

--- app/src/components/List/List.jsx
+++ app/src/components/List/List.jsx
@@ -1,10 +1,54 @@
-import React from 'react'; 
+import React,{useState}from 'react'; 
+import { CircularProgress,Grid,Typography,InputLabel,MenuItem,FormControl,Select } from '@material-ui/core';
+import PlaceDetails from '../../components/PlaceDetails/PlaceDetails';
 
+import useStyles from './styles'
 const List=()=>{
+  const classes=useStyles();
+  const [type,setType]=useState('restraunts');
+  const [rating,setRating]=useState('')
+
+  const places=[
+    {name:'cool place'},
+    {name:'steak house'},
+    {name:'Best Steak'},
+    {name:'cool place'},
+    {name:'steak house'},
+    {name:'Best Steak'},
+    {name:'cool place'},
+    {name:'steak house'},
+    {name:'Best Steak'},
+  ];
     return(
-        <h1>
-          List
-        </h1>
+      <div className={classes.container}>
+        <Typography variant="h4">
+          Restaurants ,Hotels and Attractions around you    
+        </Typography>
+        <FormControl className={classes.formControl}>
+          <InputLabel>Type</InputLabel>
+          <Select value={type} onChange={(e)=>setType(e.target.value)}>
+            <MenuItem value="restraunts">Restaurants</MenuItem>
+            <MenuItem value="hotels">Restaurants</MenuItem>
+            <MenuItem value="attractions">Restaurants</MenuItem>
+          </Select>
+        </FormControl>
+        <FormControl className={classes.formControl}>
+          <InputLabel>Rating</InputLabel>
+          <Select value={rating} onChange={(e)=>setType(e.target.value)}>
+            <MenuItem value={0}>All</MenuItem>
+            <MenuItem value={3}>Above 3</MenuItem>
+            <MenuItem value={4}>Above 4</MenuItem>
+            <MenuItem value={4.5}>Above 4.5</MenuItem>
+          </Select>
+        </FormControl>
+        <Grid container spacing={3} className={classes.list}>
+          {places?.map((place,i)=>(
+            <Grid item key={i} xs={12}>
+              <PlaceDetails place={place}/>
+            </Grid>
+          ))}
+        </Grid>
+      </div>
     )
 }
 export default List;
