49a2e5094d45bef942fad88690a9d93bcf680f3d
amolTelang <telangamol0909@gmail.com>
2021-08-08 17:56:18 +0530

    added scroll feature bug fixes

--- app/src/App.js
+++ app/src/App.js
@@ -13,7 +13,8 @@ const App=()=>{
     const [places,setPlaces]=useState([]);
     const [coordinates,setCoordinates]=useState({});
     const [bounds,setBounds]=useState({});
-
+    const [childClicked,setChildClicked]=useState(null);
+    const [loading,setIsLoading]=useState(false)
     useEffect(() => {
         navigator.geolocation.getCurrentPosition(({ coords: { latitude, longitude } }) => {
           setCoordinates({ lat: latitude, lng: longitude });
@@ -21,11 +22,11 @@ const App=()=>{
       }, []);
 
     useEffect(()=>{
-        
+            setIsLoading(true);
             getPlacesData(bounds.sw,bounds.ne)
             .then((data)=>{
                 setPlaces(data);
-                console.log(data)
+                setIsLoading(false);
             })
     },[coordinates,bounds])
     return(
@@ -34,14 +35,17 @@ const App=()=>{
             <Header/>
             <Grid container spacing={3} style={{width:'100%'}}>
                 <Grid item xs={12} md={4}>
-                    <List places={places}/>
+                    <List places={places}
+                    childClicked={childClicked}
+                    isLoading={loading}/>
                 </Grid>
                 <Grid item xs={12} md={8}>
                     <Map
                     setCoordinates={setCoordinates}
                     setBounds={setBounds}
                     coordinates={coordinates}
-                    places={places}/>
+                    places={places}
+                    setChildClicked={setChildClicked}/>
 
                 </Grid>
                 
--- app/src/components/List/List.jsx
+++ app/src/components/List/List.jsx
@@ -1,18 +1,23 @@
-import React,{useState}from 'react'; 
+import React,{useState,useEffect,createRef}from 'react'; 
 import { CircularProgress,Grid,Typography,InputLabel,MenuItem,FormControl,Select } from '@material-ui/core';
 import PlaceDetails from '../../components/PlaceDetails/PlaceDetails';
 
 import useStyles from './styles'
-const List=({places})=>{
+const List=({places,childClicked,isLoading})=>{
   const classes=useStyles();
   const [type,setType]=useState('restraunts');
   const [rating,setRating]=useState('')
+  const [elRefs,setElRefs]=useState([]);
 
+  useEffect(() => {
+    setElRefs((refs) => Array(places?.length).fill().map((_, i) => refs[i] || createRef()));
+  }, [places])
     return(
       <div className={classes.container}>
         <Typography variant="h4">
           Restaurants ,Hotels and Attractions around you    
         </Typography>
+        {isLoading ? (<div className={classes.loading}><CircularProgress size="5rem"/></div>):(<>
         <FormControl className={classes.formControl}>
           <InputLabel>Type</InputLabel>
           <Select value={type} onChange={(e)=>setType(e.target.value)}>
@@ -33,10 +38,14 @@ const List=({places})=>{
         <Grid container spacing={3} className={classes.list}>
           {places?.map((place,i)=>(
             <Grid item key={i} xs={12}>
-              <PlaceDetails place={place}/>
+              <PlaceDetails place={place}
+              selected={Number(childClicked)===1}
+              refProp={elRefs}/>
             </Grid>
           ))}
         </Grid>
+        </>
+        )}
       </div>
     )
 }
