c9b751a0f030a14f7ebaad0ee8d1f33249804773
amolTelang <telangamol0909@gmail.com>
2021-08-11 19:27:09 +0530

    added weather api removed bugs

--- app/src/components/Header/Header.js
+++ app/src/components/Header/Header.js
@@ -1,15 +1,15 @@
-import React from 'react'; 
-import {Autocomplete} from '@react-google-maps/api';
-import { AppBar,Toolbar,Typography,InputBase,Box } from '@material-ui/core';
-import SearchIcon from '@material-ui/icons/Search'
+import React from 'react';
+import { Autocomplete } from '@react-google-maps/api';
+import { AppBar, Toolbar, Typography, InputBase, Box } from '@material-ui/core';
+import SearchIcon from '@material-ui/icons/Search';
 
-import useStyles from './styles';
+import useStyles from './styles.js';
 
-const Header=()=>{
-    const classes=useStyles();
+const Header = ({ onPlaceChanged, onLoad }) => {
+  const classes = useStyles();
 
-    return(
-        <AppBar position="static">
+  return (
+    <AppBar position="static">
       <Toolbar className={classes.toolbar}>
         <Typography variant="h5" className={classes.title}>
           Travel Advisor
@@ -18,18 +18,18 @@ const Header=()=>{
           <Typography variant="h6" className={classes.title}>
             Explore new places
           </Typography>
-          {/* <Autocomplete > */}
+          <Autocomplete onLoad={onLoad} onPlaceChanged={onPlaceChanged}>
             <div className={classes.search}>
               <div className={classes.searchIcon}>
                 <SearchIcon />
               </div>
               <InputBase placeholder="Search…" classes={{ root: classes.inputRoot, input: classes.inputInput }} />
             </div>
-          {/* </Autocomplete> */}
+          </Autocomplete>
         </Box>
       </Toolbar>
     </AppBar>
-    )
-}
+  );
+};
 
 export default Header;
--- app/src/components/PlaceDetails/PlaceDetails.js
+++ app/src/components/PlaceDetails/PlaceDetails.js
@@ -1,21 +1,24 @@
-import React from 'react'; 
-import {Box,Typography,Card,Button,CardMedia,CardContent,CardActions,Chip} from '@material-ui/core'
-import LocationOnIcon from '@material-ui/icons/LocationOn'
-import PhoneIcon from '@material-ui/icons/Phone'
-import Rating from '@material-ui/lab/Rating'
+import React from 'react';
+import { Box, Typography, Button, Card, CardMedia, CardContent, CardActions, Chip } from '@material-ui/core';
+import LocationOnIcon from '@material-ui/icons/LocationOn';
+import PhoneIcon from '@material-ui/icons/Phone';
+import Rating from '@material-ui/lab/Rating';
 
-import useStyles from './styles'
-const PlaceDetails=({place,selected,refProp})=>{
-  const classes=useStyles();
-  if(selected)refProp?.current?.scrollIntoView({behavior:"smooth",block:"start"})
-    return(
-        <Card elevation={6}>
-          <CardMedia
-          style={{height:350}}
-          image={place.photo ? place.photo.images.large.url :'https://www.foodserviceandhospitality.com/wp-content/uploads/2016/09/Restaurant-Placeholder-001.jpg'}
-          title={place.name}/>
-          <CardContent>
-          <Typography gutterBottom variant="h5">{place.name}</Typography>
+import useStyles from './styles.js';
+
+const PlaceDetails = ({ place, selected, refProp }) => {
+  if (selected) refProp?.current?.scrollIntoView({ behavior: 'smooth', block: 'start' });
+  const classes = useStyles();
+
+  return (
+    <Card elevation={6}>
+      <CardMedia
+        style={{ height: 350 }}
+        image={place.photo ? place.photo.images.large.url : 'https://www.foodserviceandhospitality.com/wp-content/uploads/2016/09/Restaurant-Placeholder-001.jpg'}
+        title={place.name}
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h5">{place.name}</Typography>
         <Box display="flex" justifyContent="space-between" my={2}>
           <Rating name="read-only" value={Number(place.rating)} readOnly />
           <Typography component="legend">{place.num_reviews} review{place.num_reviews > 1 && 's'}</Typography>
@@ -33,7 +36,7 @@ const PlaceDetails=({place,selected,refProp})=>{
           </Typography>
         </Box>
         {place?.awards?.map((award) => (
-          <Box my={1} display="flex" justifyContent="space-between" my={1} alignItems="center">
+          <Box display="flex" justifyContent="space-between" my={1} alignItems="center">
             <img src={award.images.small} />
             <Typography variant="subtitle2" color="textSecondary">{award.display_name}</Typography>
           </Box>
@@ -51,8 +54,8 @@ const PlaceDetails=({place,selected,refProp})=>{
             <PhoneIcon /> {place.phone}
           </Typography>
         )}
-          </CardContent>
-          <CardActions>
+      </CardContent>
+      <CardActions>
         <Button size="small" color="primary" onClick={() => window.open(place.web_url, '_blank')}>
           Trip Advisor
         </Button>
@@ -60,8 +63,8 @@ const PlaceDetails=({place,selected,refProp})=>{
           Website
         </Button>
       </CardActions>
-        </Card>
-    )
-}
+    </Card>
+  );
+};
 
 export default PlaceDetails;
